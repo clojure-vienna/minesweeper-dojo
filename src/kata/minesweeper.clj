a87499dd67b2ab5b38f019c9aa43be6d86772664
Thomas Scheiblauer <tom@sharkbay.at>
2015-06-17 22:21:08 +0200

    add solution

---
+++ src/kata/minesweeper.clj
@@ -0,0 +1,59 @@
+(ns kata.minesweeper
+  (:require [clojure.string :as str])
+  (:gen-class))
+
+(def neighbour-coords [[-1 -1]
+                      [-1 0]
+                      [-1 1]
+                      [0 -1]
+                      [0 1]
+                      [1 -1]
+                      [1 0]
+                      [1 1]])
+
+(defn read-input [src]
+  (slurp src))
+
+(defn split-sections [input]
+  (str/split input #"\n" 2))
+
+(defn parse-body [body]
+  (let [rows (str/split body #"\n")]
+    (into [] (map (fn [row]
+                    (into [] (map #(if (= % \x) 1 0) row)))
+                  rows))))
+
+;(defn parse-header [src]
+;  [3 3])
+
+
+(defn neighbourbombs [matrix y x]
+  (reduce (fn [result [offset-y offset-x]]
+            (+ result (get-in matrix [(+ y offset-y) (+ x offset-x)] 0)))
+          0
+          neighbour-coords))
+
+(defn solve [input]
+  (map-indexed (fn [y row]
+                 (map-indexed (fn [x val]
+                                (if (= val 0) (neighbourbombs input y x) \B))
+                              row))
+               input))
+
+(defn serialize [input]
+  (str/join \newline
+            (map (fn [val] (str/join \space
+                                     (map #(if (= % 0) \space %) val)))
+                 input)))
+
+(defn -main [src-file dest-file]
+  (let [input (read-input src-file)
+        [header body] (split-sections input)
+        ;[cols rows] (parse-header header)
+        input-matrix (parse-body body)
+        solution (solve input-matrix)
+        solution-str (serialize solution)]
+    (spit dest-file solution-str)
+    ))
+
+;(-main "input.txt" "solution.txt")
